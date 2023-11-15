a0a2e125071a25a351cea4e239161426a7c3c83e
Deep Whale <103934114+ItsTheWhale@users.noreply.github.com>
2023-11-15 17:54:27 +0800

    Multiline!

--- mwhalesay.js
+++ mwhalesay.js
@@ -9,9 +9,36 @@ if (Deno.args.length) {
     }
 }
 
-console.log(` ${'_'.repeat(message.length + 2)}
-< ${message} >
- ${'-'.repeat(message.length + 2)}
+let lines = message.trim().split('\n');
+let maxLineWidth = 0;
+
+if (lines.length === 1) {
+    maxLineWidth = lines[0].length;
+    lines[0] = "< " + lines[0] + " >";
+} else {
+    for (const i in lines) {
+        lines[i] = lines[i].trim();
+        if (lines[i].length > maxLineWidth) maxLineWidth = lines[i].length;
+    }
+    for (const i in lines) {
+        lines[i] = ((i === '0') ?
+            '/'
+            : (i === String(lines.length - 1) ?
+                '\\'
+                : '|'
+            )) + ' '.repeat(Math.ceil((maxLineWidth - lines[i].length) / 2) + 1) + lines[i] + ' '.repeat(Math.floor((maxLineWidth - lines[i].length) / 2) + 1) + ((i === '0') ?
+                '\\'
+                : (i === String(lines.length - 1) ?
+                    '/'
+                    : '|'
+                ));
+    }
+}
+
+console.log(`
+ ${'_'.repeat(maxLineWidth + 2)}
+${lines.join('\n')}
+ ${'-'.repeat(maxLineWidth + 2)}
      \\ /     
   ____|____   ____
  /         \\  \\  /
