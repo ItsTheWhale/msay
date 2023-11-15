a9d16afd4b83097699cdc4e43ab3398b44912796
Deep Whale <103934114+ItsTheWhale@users.noreply.github.com>
2023-11-15 18:52:09 +0800

    Jelly say

---
+++ mjellysay.js
@@ -0,0 +1,49 @@
+let message = "";
+
+if (Deno.args.length) {
+    message = Deno.args.join(' ');
+} else {
+    const decoder = new TextDecoder();
+    for await (const chunk of Deno.stdin.readable) {
+        message += decoder.decode(chunk);
+    }
+}
+
+let lines = message.trim().split('\n');
+let maxLineWidth = 0;
+
+if (lines.length === 1) {
+    maxLineWidth = lines[0].length;
+    lines[0] = "< " + lines[0] + " >";
+} else {
+    for (const i in lines) {
+        lines[i] = lines[i].trim();
+        if (lines[i].length > maxLineWidth) maxLineWidth = lines[i].length;
+    }
+    for (const i in lines) {
+        lines[i] = ((i === '0') ?
+            '/'
+            : (i === String(lines.length - 1) ?
+                '\\'
+                : '|'
+            )) + ' '.repeat(Math.ceil((maxLineWidth - lines[i].length) / 2) + 1) + lines[i] + ' '.repeat(Math.floor((maxLineWidth - lines[i].length) / 2) + 1) + ((i === '0') ?
+                '\\'
+                : (i === String(lines.length - 1) ?
+                    '/'
+                    : '|'
+                ));
+    }
+}
+
+console.log(`
+ ${'_'.repeat(maxLineWidth + 2)}
+${lines.join('\n')}
+ ${'-'.repeat(maxLineWidth + 2)}
+    _____
+   /     \\
+ /         \\
+| _________ |
+\\/| | | | |\\/
+  / | | | \\
+ |  /   \\  |
+`);
